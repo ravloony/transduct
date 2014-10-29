b86522a5810bdda4e880aef797198d4b1c5bd312
Tom Macdonald <tm@thomas-macdonald.com>
2014-10-29 11:15:20 +0100

    make code slightly DRYer

--- src/Ravloony/Transduct/Transduct.php
+++ src/Ravloony/Transduct/Transduct.php
@@ -22,21 +22,22 @@ class Transduct {
 	 * @param string $directory the directory to get the langs from.
 	 * @return array recursive array containing all the arrays of all the lang files in $directory.
 	 */
-	public function get($directory) {
-		if (!Cache::has(self::CACHE_KEY.$directory) || Config::get('app.debug')) {
-			$this->refreshCache($directory);
+    public function get($directory) {
+		$directoryKey = self::CACHE_KEY.$directory;
+		if (!Cache::has($directoryKey) || Config::get('app.debug')) {
+			$this->refreshCache($directory, $directoryKey);
 		}
-		return Cache::get(self::CACHE_KEY.$directory);
+		return Cache::get($directoryKey);
 	}
 
-	private function refreshCache($directory) {
+	private function refreshCache($directory, $directoryKey) {
 		$locale = App::getLocale();
 		$langs = $this->buildLangArray( app_path() . '/lang/' . $locale . '/' . $directory );
 		$flags = 0;
 		if (Config::get('app.debug')) {
 			$flags |= JSON_PRETTY_PRINT;
 		}
-		Cache::forever(self::CACHE_KEY.$directory, json_encode($langs, $flags));
+		Cache::forever($directoryKey, json_encode($langs, $flags));
 	}
 
 	private function buildLangArray($directory) {
