e108ea43b0c6f2c7b2ad8a01a82ec8c053fbec3c
Dritan Latifi <dritan.latifi@motrada.net>
2019-09-30 15:24:57 +0200

    fixing the wrong method "singleton" call at ServiceProvider

--- src/Ravloony/Transduct/TransductServiceProvider.php
+++ src/Ravloony/Transduct/TransductServiceProvider.php
@@ -18,7 +18,7 @@ class TransductServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app['transduct'] = $this->app->singleton(function($app)
+		$this->app['transduct'] = $this->app->singleton('transduct', function($app)
 		{
 			return new Transduct;
 		});
